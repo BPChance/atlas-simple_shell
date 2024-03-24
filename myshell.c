d9e2f4a7ea03b28df086200d3d2048dd896734d1
root <bradenpchance6@gmail.com>
2024-03-24 00:52:50 +0000

    I added command line interpreter functionalities and necessary header files. Additional functionalities still needed.

--- myshell.c
+++ myshell.c
@@ -6,6 +6,8 @@
 #define MAX_LENGTH 1024
 int main(void)
 {
+	pid_t pid;
+	int i;
 	char input[MAX_LENGTH];
 	/** Infinite loop to prompt user til they exit */
 	while (1)
@@ -13,13 +15,45 @@ int main(void)
 		printf("$ ");
 		fflush(stdout);
 
-		/** if fgets fails it will print an error message and a failure status */
+		/** prints a newline if ctrl-d (eof) is encountered */
 		if (fgets(input, MAX_LENGTH, stdin) == NULL)
 		{
-			perror("fgets failed");
+			printf("\n");
+			break;
+		}
+		/** removes newline from input */
+		for (i = 0; input[i] != '\0'; i++)
+		{
+			if (input[i] == '\n')
+			{
+				input[i] = '\0';
+				break;
+			}
+		}
+		/** fork a child process */
+		pid = fork();
+		
+		if (pid < 0)
+		{
+			/** error handling if the fork fails */
+			perror("fork");
 			exit(EXIT_FAILURE);
 		}
-		printf("You entered: %s", input);
+		else if (pid == 0)
+		{
+			/** child process
+			 * execute the command */
+			execlp(input, input, NULL);
+			/** if execlp returns, the command couldnt be executed */
+			fprintf(stderr, "%s: command not found\n", input);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			/** parent process
+			 * wait for the child process to finish */
+			wait(NULL);
+		}
 	}
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -3,6 +3,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(void);
 
